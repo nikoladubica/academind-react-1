bee26431adf63116912a0bdbcf4e0fc1c8620e46
Nikola Cucukovic <c-nikola@hotmail.com>
2022-08-24 10:57:09 +0200

    Replacing broken chars from a response

--- src/components/Search.js
+++ src/components/Search.js
@@ -9,7 +9,9 @@ export default class Search extends React.Component {
             originalItems: [],
             items: [],
             loadedData: false,
-            value: ''
+            shownData: false,
+            value: '',
+            button: 'Get Data'
         };
     }
 
@@ -21,24 +23,54 @@ export default class Search extends React.Component {
     }
   
     getData = () => {
-        if (this.state.loadedData == false) {
+        if (this.state.loadedData === false) {
             axios.get(`https://mocki.io/v1/04350505-d817-41ee-bcf9-e4492c83b484`).then(res => {
-                const items = res.data;
+                let items = res.data;
+
+                items.forEach(item => {
+                    item.item.name = this.swapBrokenChars(item.item.name);
+                });
+
                 this.setState({
                     originalItems: items,
                     items: items,
-                    loadedData: true
+                    loadedData: true,
+                    shownData: true,
+                    button: 'Hide Data'
                 });
             });
-
-            this.state.loadedData = true;
+        } else {
+            if (this.state.shownData === true) {
+                this.setState({
+                    items: [],
+                    shownData: false,
+                    button: 'Show Data'
+                });
+            } else {
+                this.setState({
+                    items: this.state.originalItems.filter(item => item.item.name.toLowerCase().includes(this.state.value.toLowerCase())),
+                    shownData: true,
+                    button: 'Hide Data'
+                });
+            }
         }
+    }
+
+    swapBrokenChars = (string) => {
+        const arrayOfBroken = ['Ã¼', 'â€“', 'â€™', '&amp;', 'Ã¨', 'Ã©', 'Ã¶', 'Ã´', 'Ã‰', 'Â', 'Ã¯'];
+        const arrayOfFixed = ['ü', '–', '’', '&', 'è', 'è', 'ö', 'ô', 'É', '', 'ï'];
+
+        arrayOfBroken.forEach((item, index) => {
+            string = string.replaceAll(item, arrayOfFixed[index]);
+        });
+
+        return string;
     }
   
     render() {
       return (
         <div className='search'>
-            <input  className='search__get-button' onClick={this.getData} type='button' value='Get Data'  />
+            <input  className='search__get-button' onClick={this.getData} type='button' value={this.state.button}  />
             <input className='search__input' value={this.state.value} onChange={this.changeInput} type='text' placeholder='Search...'  />
 
             <ul className='search__list'>
